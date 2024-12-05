4d12ec8bde671e5c042271e0d199380ee56a30d0
d.abdulaxatov <d.abdulaxatov@hamkorbank.uz>
2024-12-05 13:45:58 +0500

    added updating user info

--- admin_panel/admin_panel/settings.py
+++ admin_panel/admin_panel/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "postgres",
         'USER': "postgres",
-        'PASSWORD': "2001",
+        'PASSWORD': "postgres",
         'HOST': "localhost",
         'PORT': 5432,
     }
